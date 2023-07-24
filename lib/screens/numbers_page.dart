6aaec2938cec4015f617a9d3bac90ee39ed37513
Ahmed Sanad <ahmed.hac26@gmail.com>
2023-07-24 03:26:58 +0300

    Number_category

--- lib/screens/numbers_page.dart
+++ lib/screens/numbers_page.dart
@@ -17,6 +17,48 @@ class Numbers extends StatelessWidget {
         title: const Text('Numbers'),
         titleSpacing: 0.0,
       ),
+      body: Container(
+        height: 100,
+        color: const Color(0xfff99531),
+        child: Row(
+          children: [
+            Container(
+                color: Color(0xfffff6dc),
+                child: Image.asset('assets/images/numbers/number_one.png')),
+            const Padding(
+              padding: EdgeInsets.only(left: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'ichi',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                  Text(
+                    'one',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const Spacer(),
+            const Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: Icon(
+                Icons.play_arrow,
+                color: Colors.white,
+                size: 30.0,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
